23f8408ddbe45ed669c7dbaca94b2e5d6fb11e5f
Mariana Barnett <marianabarnett3@gmail.com>
2020-04-25 22:45:25 -0400

    questions

--- index.js
+++ index.js
@@ -1,19 +1,81 @@
 
 // -- They give us an ARRAY called 'questions' What could we do with this (?) -- //
 const questions = [
-    "username",
-    "At least one badge",
-    "Project title",
-    "Description",
-    "Table of Contents",
-    "Installation",
-    "Usage",
-    "License",
-    "Contributing",
-    "Tests",
-    "Questions",
-    "User GitHub profile picture",
-    "User GitHub email",
+    {
+        type: "input",
+        message: "What is your user name?",
+        name: "username"
+    },
+{
+        type: "input",
+        message: "What is your badge?",
+        name: "badge"
+},
+
+{
+    type: "input",
+    message: "What is the project title?",
+    name: "title"
+},
+
+{
+    type: "input",
+    message: "Input the project description.",
+    name: "description"
+},
+
+{
+    type: "input",
+    message: "Input Table of Contents.",
+    name: "contents"
+},
+
+{
+    type: "input",
+    message: "Input installation intructions.",
+    name: "description"
+},
+{
+    type: "input",
+    message: "Input usage.",
+    name: "description"
+},
+
+{
+    type: "input",
+    message: "License.",
+    name: "description"
+},
+
+{
+    type: "input",
+    message: "Contributers.",
+    name: "description"
+},
+{
+    type: "input",
+    message: "input tests ",
+    name: "description"
+},
+   
+{
+    type: "input",
+    message: "Enter questions.",
+    name: "description"
+},
+   
+{
+    type: "input",
+    message: "Add your GitHub profile picture.",
+    name: "description"
+},
+
+{
+    type: "input",
+    message: "Enter you GitHub email.",
+    name: "description"
+},
+    
 ];
 
 // -- They give us a writeToFile() FUNCTION, Looks like we may need to read/write to a file. What BUILT-IN node module will help us out with this (?) -- // 
@@ -30,13 +92,42 @@ function writeToFile(fileName, data) {
 function init() {
     // prompt questions for inputs
     for (let i = 0; i < questions.length; i++) {
-        console.log (questions[i]);
+        console.log(questions[i]);
         // const element = questions[i];
-        
+
     }
-    var data = {};
-    // write to readme
-    // writeToFile("readme.md", data);
+    var inquirer = require("inquirer");
+    var generateMarkdown = require("utils/generateMarkdown")
+    inquirer
+        .prompt(
+            questions
+            //     {
+            //       type: "input",
+            //       message: "What is your user name?",
+            //       name: "username"
+            //     },
+            //     {
+            //       type: "password",
+            //       message: "What is your password?",
+            //       name: "password"
+            //     },
+            //     {
+            //       type: "password",
+            //       message: "Re-enter password to confirm:",
+            //       name: "confirm"
+            //     }
+            //   ]
+        )
+        .then(function (response) {
+            // if (response.confirm === response.password) {
+            //     console.log("Success!");
+            //   }
+            console.log(JSON.stringify(response))
+            // var data = generateMarkdown(response)
+            // write to readme
+            writeToFile("example.md", response);
+        });
+
 }
 
 // -- We DEFINED our INITALIZATION FUNCTION above, here we are just kicking off (running) our program. -- // 
